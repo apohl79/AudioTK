79dd4fe086334eccbbaf5410f169f8aa6686bd73
Andreas Pohl <pohl@adtech.de>
2016-10-27 17:43:45 +0200

    Triode3Filter: Use Leach's grid current model, cleanup

--- ATK/Preamplifier/Triode3Filter.cpp
+++ ATK/Preamplifier/Triode3Filter.cpp
@@ -13,7 +13,7 @@ Triode3Filter<DataType>::Triode3Filter(Triode3Type type) : Parent(1, 1) {
 
 template <typename DataType>
 Triode3Filter<DataType>::Triode3Filter(Triode3Type type, DataType Ri, DataType Rk, DataType Rg, DataType Ra,
-                                       DataType Ci, DataType Ck, DataType Co, DataType Vk, DataType Vb, DataType Kgk)
+                                       DataType Ci, DataType Ck, DataType Co, DataType Vk, DataType Vb)
     : Parent(1, 1) {
   init_tube(type);
   m_Ri = Ri;
@@ -25,21 +25,15 @@ Triode3Filter<DataType>::Triode3Filter(Triode3Type type, DataType Ri, DataType R
   m_Co = Co;
   m_Vk = Vk;
   m_Vb = Vb;
-  m_Kgk = Kgk;
   init_state();
 }
 
 template <typename DataType>
 void Triode3Filter<DataType>::setup() {
   Parent::setup();
+  setup_process_functions();
   m_T = 1. / input_sampling_rate;
-  using namespace std::placeholders;
-  if (m_use_newton) {
-    m_Vak_func = std::bind(&Triode3Filter<DataType>::Vak_newton, this, _1, _2, _3);
-  } else {
-    m_Vak_func = std::bind(&Triode3Filter<DataType>::Vak_exact, this, _1, _2, _3);
-  }
-  m_out_func = std::bind(&Triode3Filter<DataType>::process_output_final, this, _1, _2, _3, _4, _5);
+  // initial state
   Result r;
   process(0, m_Ri, r);
   m_Vo_n1 = 0;
@@ -56,46 +50,16 @@ void Triode3Filter<DataType>::adjust_cathode_lp() {
 
 template <typename DataType>
 DataType Triode3Filter<DataType>::Vg(DataType Vi_n, DataType Ri) const {
-  DataType Vgk_n1 = m_Vgk_n1 < 0 ? 0 : m_Vgk_n1;
-
   // Derivatives
   DataType Vi_n_t = (Vi_n - m_Vi_n1) * input_sampling_rate;
   DataType Vk_n_t = (m_Vk_n - m_Vk_n1) * input_sampling_rate;
-  DataType Vg_n_t =
-      ((3 * m_Kgk * m_Rg * Ri * Vk_n_t * sqrt(Vgk_n1) + 2 * m_Rg * Vi_n_t) * m_Ci - 2 * m_Rg * m_Ig_n1 - 2 * m_Vg_n1) /
-      ((2 * m_Kgk * m_Rg * Ri * sqrt(Vgk_n1) + 2 * Ri + 2 * m_Rg) * m_Ci);
-
-  DataType ret = m_Vg_n1 + Vg_n_t * m_T;
-  return ret;
+  DataType Vg_n_t = (m_Ci * Vi_n_t - m_Vg_n1 / m_Rg - m_Ig_n1 + Ri * m_Ci * Vk_n_t / (m_Rg - m_Rk)) /
+                    (m_Ci * (1 + Ri / m_Rg) + Ri * m_Ci / (m_Rg - m_Rk));
+  return m_Vg_n1 + Vg_n_t * m_T;
 }
 
 template <typename DataType>
-DataType Triode3Filter<DataType>::Vak_exact(DataType g, DataType Vgk_n, DataType Vb_n) const {
-  DataType a = g / m_h3;
-  DataType b = (g / m_h2) * Vgk_n * 3;
-  DataType c = (g / m_h) * std::pow(Vgk_n, 2) * 3 + 1 / m_Ra;
-  DataType d = g * std::pow(Vgk_n, 3) - Vb_n / m_Ra;
-
-  DataType a_3 = std::pow(a, 3);
-  DataType a_14 = std::pow(a, 14);
-  DataType a_15 = a_14 * a;
-  DataType b_2 = std::pow(b, 2);
-  DataType b_3 = b_2 * b;
-
-  DataType u = std::pow(27 * std::pow(a, 8) * d - 9 * std::pow(a, 7) * b * c + 2 * std::pow(a, 6) * b_3 +
-                            3 * SQRT_OF_3 * sqrt(27 * std::pow(a, 16) * d * d - 18 * a_15 * b * c * d +
-                                                 4 * a_15 * std::pow(c, 3) + 4 * a_14 * b_3 * d - a_14 * b_2 * c * c),
-                        1. / 3);
-
-  DataType ret =
-      -(u / (3 * SQRT3_OF_2 * a_3) -
-        (SQRT3_OF_2 * (2187 * std::pow(a, 5) * c - 729 * std::pow(a, 4) * b_2)) / (2187 * a_3 * u) + b / (3 * a));
-
-  return ret;
-}
-
-template <typename DataType>
-DataType Triode3Filter<DataType>::Vak_newton(DataType g, DataType Vgk_n, DataType Vb_n) const {
+DataType Triode3Filter<DataType>::Vak(DataType g, DataType Vgk_n, DataType Vb_n) const {
   DataType a = g / m_h3;
   DataType b = (g / m_h2) * Vgk_n * 3;
   DataType c = (g / m_h) * std::pow(Vgk_n, 2) * 3 + 1 / m_Ra;
@@ -103,7 +67,7 @@ DataType Triode3Filter<DataType>::Vak_newton(DataType g, DataType Vgk_n, DataTyp
 
   DataType Vak_last = m_Vak_n1;
   DataType ret;
-  for (int i = 0; i < mMaxIterations; ++i) {
+  for (int i = 0; i < m_max_iterations; ++i) {
     ret = Vak_last - (((a * Vak_last + b) * Vak_last + c) * Vak_last + d) / ((3 * a * Vak_last + 2 * b) * Vak_last + c);
     auto diff = std::abs(Vak_last - ret);
     if (diff < 1e-5) {
@@ -116,7 +80,7 @@ DataType Triode3Filter<DataType>::Vak_newton(DataType g, DataType Vgk_n, DataTyp
 }
 
 template <typename DataType>
-void Triode3Filter<DataType>::process(DataType Vi_n, DataType Ri, Result& r) const {
+void Triode3Filter<DataType>::process(DataType Vi_n, DataType Ri, Result& r) {
   // Grid/cathode voltage
   auto Vg_n = Vg(Vi_n, Ri);
   auto Vgk_n = Vg_n - m_Vk_n1;
@@ -125,12 +89,12 @@ void Triode3Filter<DataType>::process(DataType Vi_n, DataType Ri, Result& r) con
   auto Vb_n = m_Vb - m_Vk_n1;
 
   // Tube model: anode/cathode voltage
-  auto g = m_gInf - (m_gInf - m_g0) * std::exp(Vgk_n / m_D);
+  auto g = Vgk_n < 0 ? m_gInf - (m_gInf - m_g0) * std::exp(Vgk_n / m_D) : m_g0;
   DataType Vak_n;
-  Vak_n = m_Vak_func(g, Vgk_n, Vb_n);
+  Vak_n = Vak(g, Vgk_n, Vb_n);
 
   // No grid current for negative grid/vathode volatge
-  DataType Ig_n = Vgk_n < 0 ? 0 : m_Kgk * std::pow(Vgk_n, 1.5);
+  DataType Ig_n = Vgk_n < m_Vgamma ? 0 : (Vgk_n - m_Vgamma) / (m_Rg - m_Rk);
   // No anode current for positive grid/cathode voltage
   DataType Ia_n = Vgk_n > 0 ? 0 : g * std::pow(Vak_n / m_h + Vgk_n, 3);
   DataType Ik_n = Ig_n + Ia_n;
@@ -158,32 +122,8 @@ void Triode3Filter<DataType>::process(DataType Vi_n, DataType Ri, Result& r) con
 template <typename DataType>
 void Triode3Filter<DataType>::process_impl(int64_t size) const {
   assert(input_sampling_rate == output_sampling_rate);
-  using namespace std::placeholders;
   Result r;
-  if (get_nb_input_ports() == 2) {
-    // Follower stage
-    if (m_first_sample) {
-      // As the input signal from a previous stage has a high amount of DC we have to set the last input sample to
-      // the current one for the right steady state.
-      m_Vi_n1 = converted_inputs[0][0];
-      m_first_sample = false;
-    }
-    if (get_nb_output_ports() > 1) {
-      m_out_func = std::bind(&Triode3Filter<DataType>::process_output_pre, this, _1, _2, _3, _4, _5);
-      process_loop_pre_follower(size, r);
-    } else {
-      m_out_func = std::bind(&Triode3Filter<DataType>::process_output_final, this, _1, _2, _3, _4, _5);
-      process_loop_follower(size, r);
-    }
-  } else {
-    if (get_nb_output_ports() > 1) {
-      m_out_func = std::bind(&Triode3Filter<DataType>::process_output_pre, this, _1, _2, _3, _4, _5);
-      process_loop_pre(size, r);
-    } else {
-      m_out_func = std::bind(&Triode3Filter<DataType>::process_output_final, this, _1, _2, _3, _4, _5);
-      process_loop_single(size, r);
-    }
-  }
+  m_loop_func(size, r);
 }
 
 template class Triode3Filter<float>;
--- ATK/Preamplifier/Triode3Filter.h
+++ ATK/Preamplifier/Triode3Filter.h
@@ -17,7 +17,7 @@
  *                                      / _|_ \      Co      |
  *         +-----+     ||           Vg |       |             |
  * Vi o----|     |-----||------o-------+-..... |             |
- *         +-----+     ||      |       | .---. | ECC82       |
+ *         +-----+     ||      |       | .---. | Tube        |
  *            Ri       Ci      |        \|_ _ /            +---+
  *                             |         |                 |   |
  *                           +---+    Vk o---------+       |   | Rg
@@ -41,6 +41,8 @@
 
 namespace ATK {
 
+using namespace std::placeholders;
+
 struct Triode3TypeParams {
   // Tube model
   double g0;
@@ -56,9 +58,10 @@ struct Triode3TypeParams {
               // See adjust_cathode_lp().
 };
 
-/// Each type needs to have a parameter set at the type's index in the TubeMap.
+/// Each type has a parameter set at the type's index in the TubeMap.
 enum Triode3Type : uint8_t { ECC82 = 0, ECC83 };
 
+/// Tube models
 const Triode3TypeParams TubeMap[] = {
     // ECC82
     {.g0 = 9.888576e-15,
@@ -130,7 +133,7 @@ public:
 
   explicit Triode3Filter(Triode3Type type = Triode3Type::ECC82);
   Triode3Filter(Triode3Type type, DataType Ri, DataType Rk, DataType Rg, DataType Ra, DataType Ci, DataType Ck,
-                DataType Co, DataType Vk, DataType Vb, DataType Kgk);
+                DataType Co, DataType Vk, DataType Vb);
 
   void setup() override final;
 
@@ -138,10 +141,10 @@ public:
 
   /// Connect another stage
   inline void connect_stage(Triode3Filter<DataType>* stage) {
-    stage->set_nb_output_ports(2);  // Pre stage mode
-    set_nb_input_ports(2);          // Follower stage mode
-    set_input_port(0, stage, 0);    // Va
-    set_input_port(1, stage, 1);    // Ri
+    stage->pre_stage(true);       // Pre stage mode
+    follower_stage(true);         // Follower stage mode
+    set_input_port(0, stage, 0);  // Va
+    set_input_port(1, stage, 1);  // Ri
   }
 
   inline void Ri(DataType v) { m_Ri = v; }
@@ -156,40 +159,51 @@ public:
     adjust_cathode_lp();
   }
   inline void Vb(DataType v) { m_Vb = v; }
-  inline void Kgk(DataType v) { m_Kgk = v; }
-
-  /// Enable/disable Newton's method to calculate the tube model instead of the exact solution.
-  inline void use_newton(bool b) {
-    m_use_newton = b;
-    using namespace std::placeholders;
-    if (m_use_newton) {
-      m_Vak_func = std::bind(&Triode3Filter<DataType>::Vak_newton, this, _1, _2, _3);
-    } else {
-      m_Vak_func = std::bind(&Triode3Filter<DataType>::Vak_exact, this, _1, _2, _3);
-    }
-  }
 
   /// Enable/disable automatic adjustment of Rk and Ck to match 2Hz cutoff based on the defined Vk.
   inline void auto_adjust_cathode_lp(bool b) { m_auto_adjust_cathode_lp = b; }
 
-  inline DataType Ri() { return m_Ri; }
-  inline DataType Rk() { return m_Rk; }
-  inline DataType Rg() { return m_Rg; }
-  inline DataType Ra() { return m_Ra; }
-  inline DataType Ci() { return m_Ci; }
-  inline DataType Ck() { return m_Ck; }
-  inline DataType Co() { return m_Co; }
-  inline DataType Vk() { return m_Vk; }
-  inline DataType Vb() { return m_Vb; }
-  inline DataType Kgk() { return m_Kgk; }
-  inline bool use_newton() { return m_use_newton; }
-  inline bool auto_adjust_cathode_lp() { return m_auto_adjust_cathode_lp; }
+  /// Enable/disable the pre stage mode.
+  inline void pre_stage(bool b) {
+    m_pre_stage = b;
+    int ports = 1;
+    if (b) {
+      ports = 2;
+    }
+    set_nb_output_ports(ports);
+    setup_process_functions();
+  }
+
+  /// Enable/disable the follower stage mode.
+  inline void follower_stage(bool b) {
+    m_follower_stage = b;
+    int ports = 1;
+    if (b) {
+      ports = 2;
+    }
+    set_nb_input_ports(ports);
+    setup_process_functions();
+  }
+
+  inline DataType Ri() const { return m_Ri; }
+  inline DataType Rk() const { return m_Rk; }
+  inline DataType Rg() const { return m_Rg; }
+  inline DataType Ra() const { return m_Ra; }
+  inline DataType Ci() const { return m_Ci; }
+  inline DataType Ck() const { return m_Ck; }
+  inline DataType Co() const { return m_Co; }
+  inline DataType Vk() const { return m_Vk; }
+  inline DataType Vb() const { return m_Vb; }
+  inline bool auto_adjust_cathode_lp() const { return m_auto_adjust_cathode_lp; }
+  inline bool pre_stage() const { return m_pre_stage; }
+  inline bool follower_stage() const { return m_follower_stage; }
 
 private:
   mutable bool m_first_sample = true;
   bool m_auto_adjust_cathode_lp = true;
-  bool m_use_newton = true;
-  constexpr static int mMaxIterations = 6;
+  bool m_pre_stage = false;
+  bool m_follower_stage = false;
+  constexpr static int m_max_iterations = 6;
 
   // Tube parameters
   DataType m_g0;
@@ -204,7 +218,7 @@ private:
   DataType m_Rg = 1000e3;
   DataType m_Ci = 10e-9;
   DataType m_Co = 10e-9;
-  DataType m_Kgk = 10e-6;
+  DataType m_Vgamma = 0.6;
 
   // Circuit parameters loaded from the TubeMap
   DataType m_Rk = 0;
@@ -214,16 +228,16 @@ private:
   DataType m_Vb = 0;
 
   // Runtime state
-  mutable DataType m_Vk_n = 0;
-  mutable DataType m_Vk_n1 = 0;
-  mutable DataType m_Vg_n1 = 0;
-  mutable DataType m_Va_n1 = 0;
-  mutable DataType m_Vo_n1 = 0;
-  mutable DataType m_Vi_n1 = 0;
-  mutable DataType m_Vgk_n1 = 0;
-  mutable DataType m_Vak_n1 = 0;
-  mutable DataType m_Ig_n1 = 0;
-  mutable DataType m_Ia_n1 = 0;
+  DataType m_Vk_n = 0;
+  DataType m_Vk_n1 = 0;
+  DataType m_Vg_n1 = 0;
+  DataType m_Va_n1 = 0;
+  DataType m_Vo_n1 = 0;
+  DataType m_Vi_n1 = 0;
+  DataType m_Vgk_n1 = 0;
+  DataType m_Vak_n1 = 0;
+  DataType m_Ig_n1 = 0;
+  DataType m_Ia_n1 = 0;
 
   struct Result {
     DataType Vo;
@@ -231,9 +245,7 @@ private:
     DataType Ri;
   };
 
-  /// Helpers
-  DataType SQRT_OF_3 = std::sqrt(3);
-  DataType SQRT3_OF_2 = std::pow(2, 1. / 3);
+  /// Time delta between two samples
   DataType m_T = 0;
 
   /// Load tube parameters from the TubeMap
@@ -250,7 +262,6 @@ private:
     m_Vk = tube.Vk;
     m_Rk = tube.Rk;
     m_Ck = tube.Ck;
-    input_delay = output_delay = 1;
   }
 
   /// Initialize the runtime state
@@ -259,6 +270,7 @@ private:
     m_Va_n1 = m_Vak_n1 = m_Vb;
     m_Vk_n = m_Vk_n1 = m_Vk;
     m_Vgk_n1 = m_Vg_n1 - m_Vk_n1;
+    input_delay = output_delay = 1;
   }
 
   /// Adjustment of Rk and Ck to match 2Hz cutoff
@@ -267,22 +279,15 @@ private:
   /// Calculate grid voltage
   DataType_ Vg(DataType Vi_n, DataType Ri) const;
 
-  /// Tube model function
-  using VakFuncType = std::function<DataType(DataType, DataType, DataType)>;
-  VakFuncType m_Vak_func;
-
-  /// Caclulate anode/kathode voltage
-  DataType_ Vak_exact(DataType g, DataType Vgk_n, DataType Vb_n) const;
-
-  /// Calculate anode/kathode voltage via Newton's method
-  DataType_ Vak_newton(DataType g, DataType Vgk_n, DataType Vb_n) const;
+  /// Tube model: Calculate anode/cathode voltage via Newton's method
+  DataType_ Vak(DataType g, DataType Vgk_n, DataType Vb_n) const;
 
   /// Output function (depends on the connected outputs)
   using OutFuncType = std::function<void(DataType, DataType, DataType, DataType, Result&)>;
-  mutable OutFuncType m_out_func;
+  OutFuncType m_out_func;
 
   /// Final output with no follower stage.
-  void process_output_final(DataType Va_n, DataType, DataType, DataType, Result& r) const {
+  void process_output_final(DataType Va_n, DataType, DataType, DataType, Result& r) {
     // Output DC blocker
     r.Vo = Va_n - m_Va_n1 + m_Vo_n1 * (1 - m_T / (m_Co * m_Rg));
     r.Va = 0;
@@ -290,7 +295,7 @@ private:
   }
 
   /// Calculate output for a follower stage.
-  void process_output_pre(DataType Va_n, DataType g, DataType Vgk_n, DataType Vak_n, Result& r) const {
+  void process_output_pre(DataType Va_n, DataType g, DataType Vgk_n, DataType Vak_n, Result& r) {
     // Pre stage mode, calculate internal resistance and pass anode voltage to next stage
     DataType Xi = 1 / (3 * g / m_h * std::pow(Vgk_n + Vak_n / m_h, 2));
     r.Ri = m_Ra * Xi / (m_Ra + Xi);
@@ -299,10 +304,14 @@ private:
   }
 
   /// Process next sample
-  void process(DataType Vi_n, DataType Ri, Result& r) const;
+  void process(DataType Vi_n, DataType Ri, Result& r);
+
+  /// Process loop function for the defined mode (pre/follower)
+  using LoopFuncType = std::function<void(int64_t, Result&)>;
+  LoopFuncType m_loop_func;
 
   /// Process loop for single stage mode
-  inline void process_loop_single(int64_t size, Result& r) const {
+  void process_loop_single(int64_t size, Result& r) {
     for (int64_t s = 0; s < size; ++s) {
       DataType Vi = converted_inputs[0][s];
       process(Vi, m_Ri, r);
@@ -311,7 +320,7 @@ private:
   }
 
   /// Process loop for pre stage, no follower
-  inline void process_loop_pre(int64_t size, Result& r) const {
+  void process_loop_pre(int64_t size, Result& r) {
     for (int64_t s = 0; s < size; ++s) {
       DataType Vi = converted_inputs[0][s];
       process(Vi, m_Ri, r);
@@ -320,8 +329,18 @@ private:
     }
   }
 
+  /// As the input signal from a previous stage has a high amount of DC we have to set the last input sample to
+  /// the current one for the right steady state.
+  inline void prepare_follower() {
+    if (m_first_sample) {
+      m_Vi_n1 = converted_inputs[0][0];
+      m_first_sample = false;
+    }
+  }
+
   /// Process loop for pre stage and follower
-  inline void process_loop_pre_follower(int64_t size, Result& r) const {
+  void process_loop_pre_follower(int64_t size, Result& r) {
+    prepare_follower();
     for (int64_t s = 0; s < size; ++s) {
       DataType Vi = converted_inputs[0][s];
       DataType Ri = converted_inputs[1][s];
@@ -332,12 +351,34 @@ private:
   }
 
   /// Process loop for follower stage, no pre
-  inline void process_loop_follower(int64_t size, Result& r) const {
+  void process_loop_follower(int64_t size, Result& r) {
+    prepare_follower();
     for (int64_t s = 0; s < size; ++s) {
       DataType Vi = converted_inputs[0][s];
       DataType Ri = converted_inputs[1][s];
       process(Vi, Ri, r);
       outputs[0][s] = r.Vo;
+    }
+  }
+
+  /// Setup the loop function pointer based on the pre/follower flags
+  inline void setup_process_functions() {
+    if (follower_stage()) {
+      if (pre_stage()) {
+        m_out_func = std::bind(&Triode3Filter<DataType>::process_output_pre, this, _1, _2, _3, _4, _5);
+        m_loop_func = std::bind(&Triode3Filter<DataType>::process_loop_pre_follower, this, _1, _2);
+      } else {
+        m_out_func = std::bind(&Triode3Filter<DataType>::process_output_final, this, _1, _2, _3, _4, _5);
+        m_loop_func = std::bind(&Triode3Filter<DataType>::process_loop_follower, this, _1, _2);
+      }
+    } else {
+      if (pre_stage()) {
+        m_out_func = std::bind(&Triode3Filter<DataType>::process_output_pre, this, _1, _2, _3, _4, _5);
+        m_loop_func = std::bind(&Triode3Filter<DataType>::process_loop_pre, this, _1, _2);
+      } else {
+        m_out_func = std::bind(&Triode3Filter<DataType>::process_output_final, this, _1, _2, _3, _4, _5);
+        m_loop_func = std::bind(&Triode3Filter<DataType>::process_loop_single, this, _1, _2);
+      }
     }
   }
 };
